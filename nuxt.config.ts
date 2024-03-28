f9fe49c9fe94a87022713810947fcf8851868b0a
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-03-27 21:43:50 -0300

    Add VueUse, Pinia and Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  modules: ["@pinia/nuxt",  '@vueuse/nuxt', '@nuxtjs/tailwindcss'],
+
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
+});
+
